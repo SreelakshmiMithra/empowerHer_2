0f4e44bf32945c42669e5eaf7ed510ef42b8bffe
SreelakshmiMithra <lakshmi.badiginchala23@gmail.com>
2025-11-23 11:45:11 +0530

    q16 added

---
+++ Assignments/Day10/q16.js
@@ -0,0 +1,18 @@
+//a) Arrow function isEven(n)
+
+
+const isEven = (n) => n % 2 === 0;
+
+// console.log(isEven(4)); 
+// console.log(isEven(3)); for example test
+
+
+
+
+
+//b) Rewrite using a ternary operator
+
+const result = marks >= 35 ? "Pass" : "Fail";
+
+ // c) Arrow function greet(name) using ternary
+ const greet = (name) => `Hello, ${name ? name : "Guest"}`;
