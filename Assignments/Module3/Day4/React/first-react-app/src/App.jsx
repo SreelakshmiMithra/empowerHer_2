cc70c39f0615224f79fa0dad5c8def0fcaec1659
SreelakshmiMithra <lakshmi.badiginchala23@gmail.com>
2025-12-15 12:56:52 +0530

    Normalize line endings

---
+++ Assignments/Module3/Day4/React/first-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FirstComponent from './FirstComponent'; // import your component
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent />  {/* render it here */}
+    </div>
+  );
+}
+
+export default App;
